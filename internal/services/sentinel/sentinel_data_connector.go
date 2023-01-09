23b7f7d2203bf078673218771ad5984fce3171e4
jackofallops <ste@hashicorp.com>
2023-01-09 15:10:27 +0000

    wip

--- internal/services/sentinel/sentinel_data_connector.go
+++ internal/services/sentinel/sentinel_data_connector.go
@@ -1,34 +1,10 @@
 package sentinel
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/sentinel/parse"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	securityinsight "github.com/tombuildsstuff/kermit/sdk/securityinsights/2022-10-01-preview/securityinsights"
 )
-
-func importSentinelDataConnector(expectKind securityinsight.DataConnectorKind) pluginsdk.ImporterFunc {
-	return func(ctx context.Context, d *pluginsdk.ResourceData, meta interface{}) (data []*pluginsdk.ResourceData, err error) {
-		id, err := parse.DataConnectorID(d.Id())
-		if err != nil {
-			return nil, err
-		}
-
-		client := meta.(*clients.Client).Sentinel.DataConnectorsClient
-		resp, err := client.Get(ctx, id.ResourceGroup, id.WorkspaceName, id.Name)
-		if err != nil {
-			return nil, fmt.Errorf("retrieving Sentinel Alert Rule %q: %+v", id, err)
-		}
-
-		if err := assertDataConnectorKind(resp.Value, expectKind); err != nil {
-			return nil, err
-		}
-		return []*pluginsdk.ResourceData{d}, nil
-	}
-}
 
 func assertDataConnectorKind(dc securityinsight.BasicDataConnector, expectKind securityinsight.DataConnectorKind) error {
 	var kind securityinsight.DataConnectorKind
